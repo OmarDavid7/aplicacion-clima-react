93b6efbe8ac9c91898cc65b05509d42e768ef817
Omar <omar3568@gmail.com>
2023-07-17 14:01:06 -0500

    aplicacion de clima en react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './weatherapp.css'
+import WeatherApp from './WeatherApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WeatherApp />
+  </React.StrictMode>,
+)
